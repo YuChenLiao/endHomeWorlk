5109a812969805f952480c7b06e2685fcf45de84
YuChenLiao <yuchenliao3@gmail.com>
2022-12-06 10:42:14 +0800

    添加图片和音乐的加载方法

--- plane.js
+++ plane.js
@@ -13,3 +13,43 @@ function Plane() {
   //游戏标记
   this.flag = 'start';
 }
+
+//组装图片路径
+Plane.prototype.loadUrl = function () {
+  //组装普通图片路径
+  var nameArr = ['bg', 'bullet', 'myplane1', 'bullet', 'enemy1', 'enemy2', 'enemy3', 'enemy4', 'win', 'lost'];
+  var commonObj = {};
+  for (var i = 0; i < nameArr.length; i++) {
+    commonObj[nameArr[i]] = "images/" + nameArr[i] + ".png";
+  }
+  //分组方便取
+  this.urlObj['common'] = commonObj;
+
+  var boom1Obj = {};
+  //组装爆炸图片路径
+  for (var j = 1; j <= 9; j++) {
+    boom1Obj[j] = "images/myplane1boom/myplane1boom" + j + ".png";
+  }
+  this.urlObj['myplane'] = boom1Obj;
+
+  for (var index = 1; index <= 4; index++) {
+    var boomObj = {};
+    //组装爆炸图片路径
+    for (var i = 1; i <= 6; i++) {
+      boomObj[i] = "images/enemy" + index + "boom/enemy" + index + "boom" + i + ".png";
+    }
+    //分组方便取
+    this.urlObj['boom' + index] = boomObj;
+  }
+
+}
+//组装音乐对象
+Plane.prototype.initMusic = function (musicObj) {
+  var keys = Object.keys(musicObj);
+  var key;
+  for (var i = 0; i < keys.length; i++) {
+    key = keys[i];
+    this[key] = musicObj[key];
+  }
+}
+
