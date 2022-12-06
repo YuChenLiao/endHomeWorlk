a6ca33822b12a93842c96a4aecf27321c78f630e
YuChenLiao <yuchenliao3@gmail.com>
2022-12-06 10:43:08 +0800

    添加背景的绘制方法和初始化方法

--- plane.js
+++ plane.js
@@ -53,3 +53,65 @@ Plane.prototype.initMusic = function (musicObj) {
   }
 }
 
+//初始化
+Plane.prototype.init = function (el, musicObj) {
+  if (!el) return;
+  this.el = el;
+  this.loadUrl();
+  this.initMusic(musicObj);
+
+  var canvas = document.createElement('canvas');//创建画布
+  canvas.style.cssText = "background:white;";
+  var W = canvas.width = 520; //设置宽度
+  var H = canvas.height = 620;//设置高度
+
+  el.appendChild(canvas);//添加到指定的dom对象中
+  this.ctx = canvas.getContext('2d');
+  this.canvas = canvas;
+  this.w = W;
+  this.h = H;
+
+  var canvas2 = document.createElement('canvas');//创建画布
+  canvas2.style.cssText = "position:absolute;left:0px;";//设置样式
+  canvas2.width = W; //设置宽度
+  canvas2.height = H;//设置高度
+  el.appendChild(canvas2);//添加到指定的dom对象中
+  this.ctx2 = canvas2.getContext('2d');
+  this.canvas2 = canvas2;
+
+  //加载图片，并回调绘制出图片（因为图片是异步加载的，所以要用回调）
+  _.imageLoad(this.urlObj, this.imgObj, this.draw.bind(this));
+
+
+}
+
+//渲染图形
+Plane.prototype.render = function () {
+  var context = this.ctx;
+  this.clearCanvas();
+  _.each(this.renderArr, function (item) {
+    item && item.render(context);
+  });
+}
+//清洗画布
+Plane.prototype.clearCanvas = function () {
+  this.ctx.clearRect(0, 0, parseInt(this.w), parseInt(this.h));
+}
+
+//绘制入口
+Plane.prototype.draw = function () {
+  this.drawBG();
+  this.render();//渲染到页面上
+
+}
+
+//绘制背景
+Plane.prototype.drawBG = function () {
+  var image, img, sx = 0, sy = 0, sWidth = 520, sHeight = 620, dx = 0, dy = 0, dWidth = 520, dHeight = 620;
+  //背景
+  image = this.imgObj['common']['bg'];
+  img = new _.ImageDraw({ image: image, sx: sx, sy: sy, sWidth: sWidth, sHeight: sHeight, dx: dx, dy: dy, dWidth: dWidth, dHeight: dHeight });
+  this.renderArr.push(img);
+}
+
+global.plane = plane;
